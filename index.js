392d20f334eb81cf939ff6b9f3a9a892b9cceecd
Dee <ajalaoyinda@gmail.com>
2024-08-28 16:59:33 +0100

    Oauth

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const mongoose = require('mongoose');
-const MongoStore = require('connect-mongo'); // For storing sessions in MongoDB
-
+const MongoStore = require('connect-mongo'); 
 require('dotenv').config();
 require('./passportConfig')
 
 // Initialize Express app
 const app = express();
 
-// Setup express-session middleware
 app.use(session({
     secret: process.env.SESSION_KEY, // Use a secure key
     resave: false,
     saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }), // Store session in MongoDB
-    cookie: { secure: false } // Set to true if using HTTPS
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }), 
+    cookie: { secure: false } 
 }));
 
-// Initialize Passport.js
+
 app.use(passport.initialize());
 app.use(passport.session());
 
