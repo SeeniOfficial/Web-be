b0fc727eaee51666857032d691ad5cc0bf0720c2
Oluwayimika <oladipupomichael9@gmail.com>
2024-09-13 10:41:14 +0100

    added swagger ui

--- index.js
+++ index.js
@@ -1,53 +1,51 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-const morgan = require('morgan'); 
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("./doc/swagger.json");
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(helmet()); 
-app.use(morgan('tiny')); 
+app.use(helmet());
+app.use(morgan("tiny"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 const globalRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
-  max: 100, 
-  message: 'Too many requests from this IP, please try again after 15 minutes'
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again after 15 minutes",
 });
 app.use(globalRateLimiter);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("MongoDB connection error:", error));
+const authRoutes = require("./routes/authRoutes");
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.error('MongoDB connection error:', error));
-  const authRoutes = require('./routes/authRoutes');
-
-app.use('/api/auth', authRoutes); 
-
-
+app.use("/api/auth", authRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).json({ message: 'Route not found' });
+  res.status(404).json({ message: "Route not found" });
 });
-
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ message: 'Internal server error' });
+  res.status(500).json({ message: "Internal server error" });
 });
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
