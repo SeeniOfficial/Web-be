61450c3ce59c388177993f7531bafdd82d5baf24
Oluwayimika <oladipupomichael9@gmail.com>
2024-09-15 21:10:03 +0100

    fix: login issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,7 +44,7 @@ exports.login = async (req, res) => {
     }
     else{
         try {
-            const user = await User.findOne({ email }).select('-password'); 
+            const user = await User.findOne({ email });
             if (!user) return res.status(404).json({ message: 'User not found' });
     
             const isMatch = await bcrypt.compare(password, user.password);
